2d286f9666edd86214f43ae07e66f804cc942e37
julien-gassmann <juliengassmann974@gmail.com>
2023-05-29 15:42:09 +0400

    setup graphql + user schema

---
+++ app/Graphql/UserResolver.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Graphql;
+
+use App\Models\User;
+
+class UserResolver
+{
+    public function country(User $user): string
+    {
+        return $user->country->value;
+    }
+    public function gender(User $user): string | null
+    {
+        return $user->gender->value ?? null;
+    }
+}
