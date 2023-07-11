94e631dca53ada534b6b5131f4e2f201d8330d07
julien-gassmann <juliengassmann974@gmail.com>
2023-07-11 09:30:54 +0200

    install pest + first test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use MakesGraphQLRequests;
+    use RefreshesSchemaCache;
+
+    protected $seed = true;
+
 }
